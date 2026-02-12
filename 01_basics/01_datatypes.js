8bf732c23e37c4adefe764dc4883f0769798bfc1
dev-devanshu15 <devanshutiwari1525@gmail.com>
2026-02-12 11:26:21 +0000

    Variables and Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+let name ="Devanshu" 
+let age= 21
+let isLoggedIn =false
+let state=null
+
+
+console.table([name, age ,isLoggedIn, state ])
+console.log (typeof "Devanshu")
+console.log (typeof state) //object
+console.log (typeof isLoggedIn)
+console.log (typeof age)
+console.log (typeof 21)
+console.log (typeof name)
+console.log (typeof null) //object
+console.log (typeof undefined)  //undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 15112504
+var accountEmail = "devanshu@email.com"
+let accountPassword ="12345678"
+acountCity ="Mumbai"
+let accountState
+
+console.log (accountId)
+console.table ([accountId , accountEmail,accountPassword,acountCity,accountState])
+
+accountEmail ="Dev@2gmail.com"
+accountPassword="1212121212"
+acountCity = "uttar pradesh"
+accountState="maharashtra"
+
+console.table ([accountId , accountEmail,accountPassword,acountCity, accountState])
+console.log (accountPassword)
+console.log (accountState)
