1b2a1ecb5a1173dab794f891f62d160c9da70bf3
dev-devanshu15 <devanshutiwari1525@gmail.com>
2026-02-12 13:22:11 +0000

    Datatype Conversion

---
+++ 01_basics/01_conversionoperations.js
@@ -0,0 +1,15 @@
+let score = false
+
+/*console.log (typeof score)
+console.log (typeof (score))*/
+
+let score1 = Number (score)
+/*
+console.log (typeof score1)
+console.log (score1)
+*/
+
+let isLoggedIn = ""
+let booleanloggedIn = Boolean(isLoggedIn)
+console.log (booleanloggedIn)
+console.log (typeof booleanloggedIn)
