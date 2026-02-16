2c87c272227b184c6e4b575e44c6038e23fc7fad
dev-devanshu15 <devanshutiwari1525@gmail.com>
2026-02-16 12:36:23 +0000

    strings

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -18,15 +18,39 @@ let main = {
 const myfun=function(){
     console.log ("hello world")
 }
-// console.log (myfun)
-// console.log (typeof myfun)
+//  console.log (myfun)
+//  console.log (typeof myfun)
+
+
 let id= Symbol("12")
 let id2= Symbol("12")
 let id3= Symbol("12")
 
-console.log (id)
-console.log (id2)
-console.log (id == id2)
-console.log (id === id2 ===id3)
-console.log (typeof id)
-console.log (typeof id2)
+// console.log (id)
+// console.log (id2)
+// console.log (id == id2)
+// console.log (id === id2 ===id3)
+// console.log (typeof id)
+// console.log (typeof id2)
+
+
+
+//Stack and Heap 
+//Stack
+let  devanshu = "coder"
+let myusername = devanshu
+myusername = "beginner"
+// console.log  (myusername)
+// console.log  (devanshu)
+
+//Heap
+let yash ={
+    position:"senior  developer",
+    age:25
+}
+
+let yash1=yash
+yash1.position ="TL"
+
+console.log (yash.position)
+console.log (yash1.position)
---
+++ 01_basics/strings.js
@@ -0,0 +1,69 @@
+const name  ="dev"
+const age =21
+// console.log (name + age)
+
+// console.log (`Hello my name is ${name} my age is  ${age}`)
+
+// const gameName ="devanshu"
+// console.log( gameName[2])
+// console.log gameName.toUpperCase()
+// console.log gameName.length
+// console.log gameName.__proto__
+// console.log gameName.charAt(2)
+// console.log gameName.indexOf("V")
+
+//substring 
+// const newGame = gameName.substring(0,5)
+// console.log (newGame)
+
+//slice
+// const newString =gameName.slice(-7,4)
+// console.log (newString)
+
+
+//trim 
+// const trimString ="     devanshuu  "
+// console.log (trimString)
+// console.log (trimString.trim())
+
+//Replace
+// const email= "devanshutiwari1511@gmail-com!"
+// console.log (email)
+// console.log (email.replace('15','25').replace('11','12'))
+
+//Includes
+// console.log (email.includes('uti'))
+
+
+//Split
+// console.log (email.split('a'))
+
+// const balance=new Number(100)
+// console.log (balance)
+// console.log (balance.toString())
+// console.log (balance.toString().length)
+// console.log (balance.toFixed(1))
+
+//toPrecision
+// const order = 120.788
+// console.log (order.toPrecision(2))
+
+// const number = 100000000000000000
+// console.log (number.toLocaleString())
+
+// console.log (Math)
+// console.log (Math.ceil(4.99))
+// console.log (Math.floor(4.99))
+// console.log (Math.min (3,6,1,6,8))
+// console.log (Math.max (3,6,1,6,8))
+// console.log (Math.round (35.678))
+// console.log (Math.abs(-9))
+
+
+// console.log (((Math.random()*10)+1) +10)
+
+const xs=10
+const xy = 20
+ console.log(Math.floor(Math.random()*(xy-xs +1)+xs))
+
+ console.log (Math.random())
