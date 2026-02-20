1507c3d349d86fad30e3e16f391aa45ef6ed1d7a
dev-devanshu15 <devanshutiwari1525@gmail.com>
2026-02-20 13:37:41 +0000

    Functions

---
+++ 03-basics/01_functions.js
@@ -0,0 +1,112 @@
+// console.log("D")
+// console.log("E")
+// console.log("V")
+// console.log("A")
+// console.log("N")
+// console.log("S")
+// console.log("H")
+// console.log("U")
+
+// function fun(){
+//     console.log("D"),
+// console.log("E"),
+// console.log("V"),
+// console.log("A"),
+// console.log("N"),
+// console.log("S"),
+// console.log("H"),
+// console.log("U")
+// }
+// console.log ("Hello")
+// fun()
+
+// function addnum(num1,num2){
+//     let result=num1+num2
+//     return result
+// }
+// const result= (addnum(3,5))
+// console.log (result)
+
+// function addnum(num1,num2){
+//     // let result=num1+num2
+//     // console.log ("deva")
+//     // return result
+//     return num1+num2
+// }
+// const result=addnum(7,8)
+
+// console.log ("result is:",result)
+
+// function loginuser(username){
+//     if (username ===undefined){
+//     console.log ("please a enter a username")
+//     return
+//     }
+
+//   if (username){
+//         return `${username} is logged in `
+
+        
+//     } 
+   
+//     if (!username){
+//         console.log ("this is null value ")
+//         return
+        
+//     }    
+// }
+
+// console.log(loginuser("dev"))
+// console.log(loginuser("devanshu"))
+// console.log(loginuser("yash"))
+// console.log(loginuser("altaf"))
+// console.log(loginuser(null))
+// console.log(loginuser("deva"))
+// console.log(loginuser("devansh"))
+// console.log(loginuser())
+
+
+// function LoginUserMessage(username){
+//     if(username==undefined){
+//         console.log ("Plese enter a username")
+//         return 
+//     }
+
+//     if (username==null){
+//         console.log ("Enter valid username")
+//         return
+//     }
+//     return`${username} just logged in`
+    
+// }
+
+// console.log (LoginUserMessage())
+// console.log (LoginUserMessage (""))
+// console.log (LoginUserMessage())
+// console.log (LoginUserMessage(null))
+// console.log (LoginUserMessage(undefined))
+
+
+// const user ={
+//     username:"devanshu",
+//     email:"dev@gmail.com",
+//     price:199
+
+// }
+
+// function object(obj){
+//     console.log(`username is ${obj.username} price is ${obj.price}`)
+// }
+// // object(user)
+
+// object({
+//     username:"Altaf",
+//     price:499
+// })
+
+
+const newarray=[200,300,400]
+function forarray(arr){
+     return arr[1]
+}
+console.log (forarray([110,1100,500]))
