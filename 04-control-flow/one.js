b21fbac212c253c6bb9f6004dcae09fbb42849f8
dev-devanshu15 <devanshutiwari1525@gmail.com>
2026-03-06 13:38:59 +0000

    Control flow and For loop

---
+++ 04-control-flow/one.js
@@ -0,0 +1,142 @@
+// const score=200
+// if (score>500){
+//     const power="fly"
+//     console.log (`the ability is ${power}`)
+// }
+
+// else {
+//     const power="walk"
+//     console.log (`the ability is ${power}`)
+// }
+
+
+
+// const balance=800
+// //its mandatory or its a good practice to pt ";" after a if line
+// if (balance<500) {
+//     console.log("smaller than 500")
+// }
+// else if (balance<700) {
+//     console.log("smaller than 700 ")
+// }
+// else if (balance<900) {
+//     console.log("Smaller than 900 ")
+// }
+// else if (1000<balance<2000){
+//     console.log ("Smaller than 2000 and Greater than 1000")
+// }
+// else{
+//    console.log ("Smaller than 500 ")
+// }
+
+
+// function isJavascriptHard(teacher){
+
+// if(teacher=="Devanshu"){
+//  console.log("Javascript is easy")
+// }
+// else{
+//   console.log("Javascript is difficult") 
+// }
+// }
+
+// isJavascriptHard("Devanshu")
+
+
+
+
+//Switch case
+// const month=1
+
+// switch (month) {
+//     case 1:
+//         console.log  ("jan")
+//         break;
+//     case 2:
+//         console.log  ("feb")
+//         break;
+//     case 3:
+//         console.log  ("march")
+//         break;
+//     case 4:
+//         console.log  ("april")
+//         break;
+//     case 5:
+//         console.log  ("may")
+//         break;
+
+//     default:
+//         console.log ("default case matched")
+//         break;
+// }
+
+
+
+// const username="Devanshu"
+
+// if (username=="Devanshu"){
+//     console.log ("Print right")
+// }else {
+//     console.log ("Print wrong")
+// }
+
+
+
+// const array=[200000, 7566, 675]
+// if(array.length==3){
+//     console.log("array is empty")
+// }
+
+// const object={}
+// if (Object.keys(object)==0){
+//     console.log("array is emptyyyy")
+
+// }
+
+
+
+
+
+//"And" operator "Or" Operator
+
+// const userLoogedIn=true
+// const debitCard=true
+// const loggedInFroGoogle=false
+// const loggedInFromEmail=true
+
+// if (userLoogedIn && debitCard){
+//     console.log ("Allow to do shoping")
+// }
+ 
+// if (loggedInFroGoogle || loggedInFromEmail){
+//     console.log ("User Looged-In")
+// }
+
+
+
+//"Nullish" Operator
+
+
+
+//Terniary Operator
+// const Blendedscotch=1500
+
+// Blendedscotch>=2000 ? console.log("It is DEWAR's"): console.log ("It is the GLEENWALK")
+
+
+
+
+// const balance=100;
+
+// if(balance>500) console.log ("Balance is less than 500")
+//     else {console.log ("yash hello")}
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
---
+++ 05-iterations/one.js
@@ -0,0 +1,56 @@
+//for
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element==5){
+//         console.log ("The values")
+//     }
+//     console.log ("value:",element)
+    
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//      console.log  (`The outer value is ${i}`)
+
+//     for (let u = 0; u <=11; u++) {
+//         // console.log (`The Inner Value is = ${u}`)
+//         //console.log (i + ' * ' +  u + ' = '+ i*u)
+//         console.log (`${i} * ${u} = ${i*u}`)
+        
+//     }
+    
+    
+//}
+
+
+
+// let myarr=["dev", "kadar","altaf"]
+
+// for (let index = 0; index < myarr.length; index) {
+//     const element = myarr[index];
+//     console.log (element);
+    
+// }
+
+
+
+//Break and Continue 
+// for (let i = 1; i < 50; i++) {
+//     if (i==25){
+//         console.log(`birthday detected ${i}`)
+//         break
+//     }
+    
+//     console.log (i)
+    
+// }
+// for (let i = 1; i < 50; i++) {
+//     if (i==25){
+//         console.log(`birthday detected ${i}`)
+//         continue
+//     }
+    
+//     console.log (i)
+    
+// }
